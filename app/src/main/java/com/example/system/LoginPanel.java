6afd92f6ecbc8782a4c0b6890128455674ee0919
Konrad <konradhbr@gmail.com>
2020-10-17 00:16:08 +0200

    Created login panel and connect to firebase

---
+++ app/src/main/java/com/example/system/LoginPanel.java
@@ -0,0 +1,100 @@
+package com.example.system;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginPanel extends AppCompatActivity {
+
+    EditText emailId, password;
+    Button btnSignIn;
+    CheckBox checkWorker;
+    CheckBox checkManager;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_panel);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSignIn = findViewById(R.id.button2);
+        checkWorker = findViewById(R.id.checkWorker);
+        checkManager = findViewById(R.id.checkManager);
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(com.example.system.LoginPanel.this,"You are logged in",Toast.LENGTH_SHORT).show();
+
+                }
+                else if( mFirebaseUser != null ){
+                    Toast.makeText(com.example.system.LoginPanel.this,"You are logged in",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(com.example.system.LoginPanel.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailId.setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(LoginPanel.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginPanel.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginPanel.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(LoginPanel.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+}
--- app/src/main/java/com/example/system/MainActivity.java
+++ app/src/main/java/com/example/system/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        login(R.layout.activity_login_panel);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+    public void login(int v) {
+        Intent login = new Intent(com.example.system.MainActivity.this, LoginPanel.class);
+        startActivity(login);
     }
 }
