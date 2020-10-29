f7537d5c0792cec47e536a5e16203a3b275eaaa7
Konrad <konradhbr@gmail.com>
2020-10-29 18:30:00 +0100

    Created button in manager's panel and charts activity

--- app/src/main/java/com/example/system/Manager.java
+++ app/src/main/java/com/example/system/Manager.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Manager extends AppCompatActivity {
-    Button messageBTN;
+    Button messageBTN ,dataBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +16,22 @@ public class Manager extends AppCompatActivity {
         setContentView(R.layout.activity_manager);
 
         messageBTN = findViewById(R.id.message);
+        dataBTN = findViewById(R.id.charts);
+
 
         messageBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mes = new Intent(com.example.system.Manager.this, MessagesList.class);
                 startActivity(mes);
+            }
+        });
+
+        dataBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent(com.example.system.Manager.this, Charts.class);
+                startActivity(data);
             }
         });
     };
